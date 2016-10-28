3530fea69ccfa9d69ce01310e69bb52641835f3c
David Herrmann <davherrmann@googlemail.com>
2016-10-29 01:07:13 +0200

    complete exercise 2-1, add mutex lock

--- exercises/2-1.c
+++ exercises/2-1.c
@@ -2,6 +2,7 @@
 #include <pthread.h>
 
 int cnt;
+pthread_mutex_t mutex;
 void nextcnt();
 
 void* start_nextcnt(void* val) {
@@ -11,21 +12,24 @@ void* start_nextcnt(void* val) {
 
 void nextcnt() {
   printf("%d\n", cnt);
+  pthread_mutex_lock(&mutex);
   if (cnt < 20) {
-    // TODO mutex lock
     cnt++;
+    pthread_mutex_unlock(&mutex);
 
     pthread_t t;
     pthread_create(&t, NULL, &start_nextcnt, NULL);
     pthread_join(t, NULL);
     printf("joined\n");
   } else {
+    pthread_mutex_unlock(&mutex);
     for (int i = 0; i < 1000; i++);
   }
 }
 
 int main(int argc, char const *argv[]) {
   cnt = 0;
+  pthread_mutex_init(&mutex, NULL);
   nextcnt();
 
   return 0;
