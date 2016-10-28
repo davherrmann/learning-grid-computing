687d7f3792d2297c4395b8596aaebcb5376555c2
David Herrmann <davherrmann@googlemail.com>
2016-10-29 00:49:07 +0200

    add exercise 2-2

---
+++ exercises/2-2.c
@@ -0,0 +1,52 @@
+#include <stdio.h>
+#include <pthread.h>
+
+float integrate(float a, float b);
+
+typedef struct {
+  float a;
+  float b;
+  float result;
+} Workload;
+
+void* start_integrate(void* val) {
+  Workload *workload = val;
+  workload->result = integrate(workload->a, workload->b);
+
+  printf("p result: %f\n", workload->result);
+
+  return NULL;
+}
+
+float f(float x) { return x * x + 1; }
+float df(float x) { return 2 * x; }
+
+float integrate(float a, float b) {
+  float m = (a + b) / 2;
+
+  // TODO user input?
+  float epsilon_1 = 0.01;
+  float epsilon_2 = 0.0625;
+
+  if (df(m) / f(m) > epsilon_1 && b - a > epsilon_2) {
+    Workload workload = {m, b};
+    pthread_t thread;
+
+    pthread_create(&thread, NULL, start_integrate, &workload);
+    float i_0 = integrate(a, m);
+
+    printf("o result: %f\n", i_0);
+
+    pthread_join(thread, NULL);
+
+    return i_0 + workload.result;
+  } else {
+    return (b - a) * f(m);
+  }
+}
+
+int main(int argc, char const *argv[]) {
+  float i = integrate(0, 1);
+  printf("%f\n", i);
+  return 0;
+}
