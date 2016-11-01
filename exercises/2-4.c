6ec1c134659663dba1ea96673b87db61ce7f689b
David Herrmann <davherrmann@googlemail.com>
2016-11-01 13:40:48 +0100

    add time output

--- exercises/2-4.c
+++ exercises/2-4.c
@@ -3,27 +3,38 @@
 
 #include <mpi.h>
 #include <stdio.h>
+#include <time.h>
 #include <unistd.h>
 
 static const int CHAR_BUFFER_SIZE = 1024;
 static const int MASTER_ID = 0;
 static const int RESULT_HOSTNAME = 0;
 
+void nodeInfo(char* info) {
+  time_t localTime;
+  char hostname[512];
+
+  localTime = time(NULL);
+  gethostname(hostname, CHAR_BUFFER_SIZE);
+
+  sprintf(info, "%s; %s", hostname, ctime(&localTime));
+}
+
 void master(const int size) {
   char buffer[CHAR_BUFFER_SIZE];
 
-  gethostname(buffer, CHAR_BUFFER_SIZE);
-  printf("%s\n", buffer);
+  nodeInfo(buffer);
+  printf("%s", buffer);
 
   for (int n = 0; n < size - 1; n++) {
     MPI_Recv(buffer, CHAR_BUFFER_SIZE, MPI_CHAR, MPI_ANY_SOURCE, RESULT_HOSTNAME, MPI_COMM_WORLD, NULL);
-    printf("%s\n", buffer);
+    printf("%s", buffer);
   }
 }
 
 void slave() {
   char buffer[CHAR_BUFFER_SIZE];
-  gethostname(buffer, CHAR_BUFFER_SIZE);
+  nodeInfo(buffer);
 
   MPI_Send(buffer, CHAR_BUFFER_SIZE, MPI_CHAR, MASTER_ID, RESULT_HOSTNAME, MPI_COMM_WORLD);
 }
